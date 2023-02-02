900537be639f5d7b93d96237ce169470d44b5d6b
cyberpaxan <igorl098765@gmail.com>
2023-02-02 16:58:07 +0500

    fixed bug with sorting pizzas

--- src/redux/slices/pizzaSlice.ts
+++ src/redux/slices/pizzaSlice.ts
@@ -7,7 +7,7 @@ export const fetchPizzas = createAsyncThunk<Pizza[], Record<string, string>>(
     async (params) => {
         const { order, sortBy, category, search, currentPage } = params;
         const { data } = await axios.get<Pizza[]>(
-            `https://63c52a1cf3a73b34784f68fb.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`
+            `https://63c52a1cf3a73b34784f68fb.mockapi.io/items?${category}&page=${currentPage}&limit=4&sortBy=${sortBy}&order=${order}${search}`
         );
 
         return data;
